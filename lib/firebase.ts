594f79d5e9348ac4a2876b5532008629c73043be
Anubhav yadav <anubhav1374@gmail.com>
2024-11-27 19:03:27 +0530

    updated

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeLcIpyppieiIGUUS_R_XeQNT9WVMXJPw",
-  authDomain: "test-app-1c11d.firebaseapp.com",
-  projectId: "test-app-1c11d",
-  storageBucket: "test-app-1c11d.firebasestorage.app",
-  messagingSenderId: "383446452595",
-  appId: "1:383446452595:web:9e6a317da9de8635bfdd06"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
